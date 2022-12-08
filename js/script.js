1105503ee8c10f688dd0f67b84b8a25c91f3db8b
Patryk Solinski <solinskipatryk8@gmail.com>
2022-12-08 18:56:54 +0100

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -11,21 +11,47 @@
         },
     ];
 
-    const render = ()=>{
-        let htmlString= "";
+    const render = () => {
+        let htmlString = "";
 
-        for(const task of tasks){
-            htmlString +=`
-            <li>
+        for (const task of tasks) {
+            htmlString += `
+            <li 
+            ${task.done ? "style = \"text-decoration: line-through\"" : ""} 
+            >
             ${task.content}
             </li>
             `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
     };
+
+    const addNewTask = (newTaskContent) =>{
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit =(event) =>{
+        event.preventDefauly();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === ""){
+            return;
+        }
+
+       addNewTask(newTaskContent);
+    };
+    
     const init = () => {
         render();
 
+        const form = doocument.querySelector(".js-form");
+
+        form.addEventListener("submit",onFormSubmit)
     };
 
     init();
